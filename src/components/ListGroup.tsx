749a9dafe5dc0e709c9f43bb3e93c2936581e823
Haithem Radjaa <haithemradjaaa@gmail.com>
2025-02-28 11:38:24 +0100

    final techniques on using ternary operator or better using the AND op to render elements

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,12 +6,7 @@ function ListGroup() {
     "Zaki",
   ];
 
-  items = [];
-
-  const getMessage = () => { // using a function is better than using a const because we can pass arguments to it for more flexibility and dynamic behavior
-    // if you don't have a complex structure of a code, just use a const
-    return items.length === 0 ? <p>no items found!</p> : null; // with this we don't have to pollute our code and make it cryptic!
-  }
+  // items = [];
 
   
   // this is an example of what it looks like to render a conditional
@@ -22,16 +17,18 @@ function ListGroup() {
     // instead of a div or something like that, we have a better solution, a fragment for multiple elements return, but we are going to have an error because in jsx we can only use html elements and react components!
     // to fix that problem, wrap that expression in '{}'
     // add the proper class to maintain the style
+    // in this example, if the list is empty, we have nothing rendered on the screen!
     <>
       <h1>Heading here for the sake of the example!</h1>
 
-      {getMessage()}      
+      { !(items.length === 0) &&       
 
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>{item}</li>
         ))}
       </ul>
+    }
     </>
   );
   // this is the compatible jsx code with the bootstrap styling brought from the bootstrap docs
