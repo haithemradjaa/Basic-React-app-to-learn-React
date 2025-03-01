4a7e23489c6ad8ccd5263575d7806b1716505dac
Haithem Radjaa <haithemradjaaa@gmail.com>
2025-03-01 12:39:32 +0100

    Solution to the exercise plus modifying the bootstrap classes dynamically plus adding a default (and optional) color value

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Button from "./components/Button";
 
 function App() {
-  return ( 
+  return (
     <div>
-      <Button>
+      <Button color="success" onClick={() => console.log("clicked")}>
         This is a button!
       </Button>
     </div>
-  )
+  );
 }
 
 export default App; // for it to be used somewhere else
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,26 @@
-import { ReactNode } from "react"
-
-interface ButtonProps{
-    children: ReactNode;
+interface ButtonProps {
+  children: string;
+  color? /** this is an optional prop, and you need to specify this to the ts compiler not to expect it when you want react to build your component */: string; // to be able to choose what color the button will have dynamically!
+  // in this color prop, the values must match what bootstrap defined, like 'primary' for blue buttons
+  onClick: () => void; // when clicked what will happen
 }
 
-const Button = ({ children }: ButtonProps) => {
+const Button = ({
+  children,
+  onClick,
+  color = "success" /* here we are setting a default value for the color prop */,
+}: ButtonProps) => {
   return (
-    <button type="button" className="btn btn-success">{children}</button>
-  )
-}
+    <button
+      type="button"
+      onClick={onClick}
+      className={
+        "btn btn-" + color
+      } /* Here we are appending the color prop to the class name to dynamically change it */
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
