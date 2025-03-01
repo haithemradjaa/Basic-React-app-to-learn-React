2dcc8981dbf31a5693e522110501a4377786f35b
Haithem Radjaa <haithemradjaaa@gmail.com>
2025-03-01 13:09:06 +0100

    Creating a button that toggles the visibility of an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisisble, SetAlertvisibility] = useState(false); // this array is for toggling the visibility of the alert component in our app
+
   return (
+    // the conditional rendering happens only when the alertVisisble = true
     <div>
-      <Button color="success" onClick={() => console.log("clicked")}>
+      {alertVisisble && <Alert>my Alert</Alert>}
+      <Button
+        color="success"
+        onClick={() =>
+          SetAlertvisibility(!alertVisisble)
+        } /* this will keep toggling the true-false values each time clicked, and that will result in updating the DOM on our page */
+      >
         This is a button!
       </Button>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+
 interface ButtonProps {
   children: string;
   color?:
